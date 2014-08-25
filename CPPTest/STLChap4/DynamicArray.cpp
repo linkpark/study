83d583f9ff761014257cd309d4ad9e9bd6b371e3
linkpark <836792834@qq.com>
2014-08-25 21:10:27 +0800

    modify code

--- CPPTest/STLChap4/DynamicArray.cpp
+++ CPPTest/STLChap4/DynamicArray.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 using namespace std;
 
@@ -26,6 +29,17 @@ protected:
         finish = start + n;
         endOfStorage = finish;
     }
+
+    void growRoom(){
+        size_t size = finish - start;
+        size_t byteSize = size * sizeof(T);
+        
+        iterator newStart = allocNewRoom(size * 2);
+        memcpy(newStart,start,byteSize);
+        delete start;
+        start = newStart;
+        endOfStorage = start + size*2; 
+    }
     
 public:
     DynamicArray(size_t n){
@@ -47,11 +61,34 @@ public:
     size_t size()const{
         return (finish - start);
     } 
+    
+    size_t length()const{
+        return (endOfStorage - start);
+    }
+    
+    void push_back(const T& x){
+        if(finish == endOfStorage){
+            growRoom();
+        }
+
+        *finish = x;
+        finish++;
+    }
+
+    reference operator [](size_t n){
+        return *(start + n);
+    }
 };
 
 int main(){
-    DynamicArray< int > a(20);
-    cout << a.size() <<endl;
+    DynamicArray< int > a(1);
+    a[0] = 1;
+    a.push_back(2);
+    a.push_back(3);
+    a.push_back(4);
+    cout << a[0] << endl;
+    cout << a.length() <<endl;
 
     return 0;
 }
+
