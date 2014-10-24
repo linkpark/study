2060f99f9b1a39dd61220f3a81520a20bb861349
linkpark <836792834@qq.com>
2014-10-24 18:28:33 +0800

    modify code

--- LPUE/homeworkDuan/WordCount/comm/TCPListenAgent.h
+++ LPUE/homeworkDuan/WordCount/comm/TCPListenAgent.h
@@ -1,4 +1,18 @@
 #ifndef TCPLISTENAGENT_H_
 #define TCPLISTENAGENT_H_
+#include "Agent.h"
+
+class TCPListenAgent:public Agent{
+public:
+    TCPListenAgent( int port, const char* pIpAddr );
+    ~TCPListenAgent();
+
+public:
+
+
+private:
+    Socket 
+    
+};
 
 #endif
