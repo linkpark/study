bf37768cc840d33aee1686661a6a55cbc4ba1dde
linkpark <836792834@qq.com>
2014-11-16 17:37:12 +0800

    add code

--- LPUE/homeworkDuan/ThreadPool/ThreadPool.h
+++ LPUE/homeworkDuan/ThreadPool/ThreadPool.h
@@ -1,3 +1,37 @@
-
+/*
+ * =================================================================
+ *
+ *            Filename:    ThreadPool.h
+ *
+ *         Description:    
+ *
+ *             Version:    v1.0
+ *             Created:    2014-11-16 17:25
+ *           Reversion:    none
+ *            Compiler:    g++
+ *            
+ *              Author:    wangjin, 836792834@qq.com
+ * 
+ * ==================================================================
+ */
+#ifndef THREADPOOL_H_
+#define THREADPOOL_H_
 #include "WorkThread.h"
-#include ""
+#include <map>
+#include <vector>
+
+class ThreadPool{
+public:
+    ThreadPool();
+    virtual ~ThreadPool();
+
+public:
+    int initial( int threadNumber );   
+    std::vector<int> applyForThreads( int threadNumber );
+
+private:
+    std::map< WorkThread*, bool > m_ThreadList;
+    int m_ThreadNumber;
+};
+
+#endif
